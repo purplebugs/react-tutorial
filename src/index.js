ce9df39b9c306d57a496f0e1c5dd051bcf3d51e7
Anita Lipsky <anita@purplebugs.com>
2021-12-30 20:21:31 +0100

    props for two books

--- src/index.js
+++ src/index.js
@@ -5,16 +5,31 @@ import ReactDom from "react-dom";
 import "./index.css";
 
 // setup vars
-const author = "Amelia Hepworth";
-const title = "I Love You to the Moon and Back";
-const img =
-  "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg";
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY436_FMwebp_QL65_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book title="random title" number={12} />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
@@ -22,12 +37,9 @@ const Book = (props) => {
   console.log(props);
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
