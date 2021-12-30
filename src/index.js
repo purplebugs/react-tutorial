04e627733465ce6d02df3fef38683164480bd166
Anita Lipsky <anita@purplebugs.com>
2021-12-30 20:37:07 +0100

    children prop, some css

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const firstBook = {
 };
 
 const secondBook = {
-  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY436_FMwebp_QL65_.jpg",
+  img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
   title: "Our Class is a Family",
   author: "Shannon Olsen",
 };
@@ -24,7 +24,13 @@ function BookList() {
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          dolorem maiores, similique architecto nihil ullam officiis facere.
+          Iure, tenetur ea.
+        </p>
+      </Book>
       <Book
         img={secondBook.img}
         title={secondBook.title}
@@ -33,12 +39,13 @@ function BookList() {
     </section>
   );
 }
-const Book = ({ img, author, title }) => {
+const Book = ({ img, author, title, children }) => {
   //const { img, author, title } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
+      {children}
       <h4>{author}</h4>
     </article>
   );
