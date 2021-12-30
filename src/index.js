cfb85603fa29e6f1608f0ee70b67e853393fb559
Anita Lipsky <anita@purplebugs.com>
2021-12-30 16:26:32 +0100

    React fragment instead of wrapping in div

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDom from "react-dom";
 
 function Greeting() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <React.Fragment>
+      <div>
+        <h3>Hello people</h3>
+        <ul>
+          <li>
+            <a href="#">Hello world</a>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
   );
 }
 
