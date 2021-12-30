b7099d33d8ae212fb3bbef66fcdbaeb170f025ed
Anita Lipsky <anita@purplebugs.com>
2021-12-30 16:14:48 +0100

    create component with arrow function instead

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return <h4>This is Anita and this is my first component</h4>;
-}
+// function Greeting() {
+//   return <h4>This is Anita and this is my first component</h4>;
+// }
+
+const Greeting = () => {
+  return React.createElement("h1", {}, "Hello world");
+};
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
