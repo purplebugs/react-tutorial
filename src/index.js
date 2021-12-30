1cdbf7506b89b7bcb92ee1d4715e5cfcb28372f4
Anita Lipsky <anita@purplebugs.com>
2021-12-30 16:16:14 +0100

    create component with function that returns two HTML elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// function Greeting() {
-//   return <h4>This is Anita and this is my first component</h4>;
-// }
+function Greeting() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
 
-const Greeting = () => {
-  return React.createElement("h1", {}, "Hello world");
-};
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello world");
+// };
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
