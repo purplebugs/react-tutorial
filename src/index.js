693de2b03dd74b77559d188d40ae0239dd0b054e
Anita Lipsky <anita@purplebugs.com>
2021-12-30 21:12:05 +0100

    key and spread operator

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import "./index.css";
 // setup vars
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "I Love You to the Moon and Back",
     author: "Amelia Hepworth",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "Our Class is a Family",
     author: "Shannon Olsen",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/41w4B0f21VL._SY349_BO1,204,203,200_.jpg",
     title: "The Very Hungry Caterpillar",
     author: "Eric Carle",
@@ -28,14 +31,14 @@ function BookList() {
     <section className="booklist">
       {books.map((book) => {
         const { img, title, author } = book;
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, author, title } = props.book;
+const Book = ({ img, author, title }) => {
+  // const { img, author, title } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
