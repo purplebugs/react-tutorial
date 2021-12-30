523d5a1e7cb7d9ea6b4242de4ddcd5bd17cbbccc
Anita Lipsky <anita@purplebugs.com>
2021-12-30 19:44:30 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
+// setup vars
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
@@ -11,18 +17,12 @@ function BookList() {
     </section>
   );
 }
-
-const author = "Amelia Hepworth";
 const Book = () => {
-  const title = "I Love You to the Moon and Back";
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
     </article>
   );
 };
