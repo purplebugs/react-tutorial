4a643f0475bf78ad0f5c61c76643a14b3febc8e4
Anita Lipsky <anita@purplebugs.com>
2021-12-30 16:19:39 +0100

    use function with return, for readability

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h1>Hello world</h1>
-//     </div>
-//   );
-// }
-
-const Greeting = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Hello world")
+function Greeting() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
   );
-};
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Hello world")
+//   );
+// };
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
