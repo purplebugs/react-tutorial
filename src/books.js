86fdae338dec7205747215e5faa0a8ea8a22e888
Anita Lipsky <anita@purplebugs.com>
2021-12-31 08:35:51 +0100

    named export - with JS file does not need .js extension

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41w4B0f21VL._SY349_BO1,204,203,200_.jpg",
+    title: "The Very Hungry Caterpillar",
+    author: "Eric Carle",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,28 +3,7 @@ import ReactDom from "react-dom";
 
 // CSS
 import "./index.css";
-
-// setup vars
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-    title: "Our Class is a Family",
-    author: "Shannon Olsen",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/41w4B0f21VL._SY349_BO1,204,203,200_.jpg",
-    title: "The Very Hungry Caterpillar",
-    author: "Eric Carle",
-  },
-];
+import { books } from "./books";
 
 function BookList() {
   return (
