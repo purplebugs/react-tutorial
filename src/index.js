210c21cb4ec58e0e0576a10b9505f8d60c9531f9
Anita Lipsky <anita@purplebugs.com>
2021-12-31 08:45:08 +0100

    export relative path

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import ReactDom from "react-dom";
 import "./index.css";
 import { books } from "./books";
 import Book from "./Book";
+import { greeting } from "./testing/testing";
 
 function BookList() {
+  console.log(greeting);
   return (
     <section className="booklist">
       {books.map((book) => {
